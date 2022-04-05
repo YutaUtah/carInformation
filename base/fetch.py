21fdac174665aa3f8b8c812a3a81d30d3dbe2b65
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-04-05 21:16:58 +0900

    added fetch.py

---
+++ base/fetch.py
@@ -0,0 +1,40 @@
+import json
+import requests
+
+from .utils import is_different_car_object, generate_car_image_dir
+
+
+class Car:
+    def __init__(self, maker, model, image_jpg):
+        self.maker = maker
+        self.model = model
+        self.image_jpg = image_jpg
+
+    def __eq__(self, other):
+        if not isinstance(other, Car):
+            return False
+        return self.maker == other.maker and self.model == other.model
+
+
+class FetchAPI:
+    def __init__(self, requested_url, headers):
+        self.requested_url = requested_url
+        self.headers = headers
+
+
+    @property
+    def __fetched_API(self):
+        response = requests.get(self.requested_url, headers=self.headers)
+        return json.loads(response.text[11:-3])
+
+
+    def get_carlist(self):
+        car_list = []
+        for each_car_info in self.__fetched_API:
+            if is_different_car_object(each_car_info, car_list):
+                car_list.append(Car(each_car_info['model_make_id'],
+                                    each_car_info['model_name'],
+                                    generate_car_image_dir(each_car_info))
+                                )
+
+        return car_list
