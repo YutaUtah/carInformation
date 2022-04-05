7e24b227762490b45a729b9c82ba88e54e3462e4
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-04-05 18:08:25 +0900

    added config_car and config_request

---
+++ base/config/config_car.py
@@ -0,0 +1,12 @@
+
+class CarMap:
+    CAR_TO_IMG = {
+        'Acura': "acura.jpg",
+        'Chevrolet': "chevrolet.jpg",
+        'Bentley': "bentley.jpg",
+        'Buick': "buick.jpg",
+        'Cadillac': "cadillac.jpg",
+        'Audi': "audi.jpg",
+        'BMW': "bmw.jpg"
+    }
+
