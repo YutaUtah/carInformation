6cea6c85608ec5d5697f4671d1c032634e675c34
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-04-05 22:45:22 +0900

    added get_car_image functions

--- base/views.py
+++ base/views.py
@@ -1,3 +1,5 @@
+import os
+
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
@@ -13,3 +15,13 @@ def index(request):
         'cars': responded_API_object.get_carlist(),
     }
     return render(request, "pages/index.html", context)
+
+def model_list(request):
+    return None
+
+def get_car_image(request):
+    files = os.listdir('base/static/img')
+    context = {
+        'files': files,
+    }
+    return render(request, "pages/carList.html", context)
