228f1554276c1ef1e2cfc8a5b4951822bf19c730
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-04-06 14:50:10 +0900

    added get_model_list func

--- base/views.py
+++ base/views.py
@@ -4,11 +4,11 @@ from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
 from .config.config_request import RequestConfig
+from .config.config_car import CarMap
 from .fetch import FetchAPI
 
+
 # Create your views here.
-
-
 def index(request):
     responded_API_object = FetchAPI(RequestConfig.REQUEST_URL.value, RequestConfig.HEADERS.value)
     context = {
@@ -16,8 +16,6 @@ def index(request):
     }
     return render(request, "pages/index.html", context)
 
-def model_list(request):
-    return None
 
 def get_car_image(request):
     files = os.listdir('base/static/img')
@@ -25,3 +23,14 @@ def get_car_image(request):
         'files': files,
     }
     return render(request, "pages/carList.html", context)
+
+
+def get_model_list(request, car_model):
+    responded_API_object = FetchAPI(RequestConfig.REQUEST_URL.value, RequestConfig.HEADERS.value)
+    car_model_list = responded_API_object.get_carlist(car_model)
+
+    context = {
+        'cars': car_model_list,
+        'car_img_jpg': CarMap.CAR_TO_IMG[car_model_list[0].maker]
+    }
+    return render(request, "pages/modelList.html", context)
