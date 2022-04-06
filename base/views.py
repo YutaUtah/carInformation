5677a87edf2ef8a36d61075d6b0d04410e9cfded
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-04-07 00:19:28 +0900

    modified variables in context in index func

--- base/views.py
+++ base/views.py
@@ -12,15 +12,21 @@ from .fetch import FetchAPI
 def index(request):
     responded_API_object = FetchAPI(RequestConfig.REQUEST_URL.value, RequestConfig.HEADERS.value)
     context = {
-        'cars': responded_API_object.get_carlist(),
+        'cars_list': responded_API_object.get_carlist(),
+        'cars_set': set(car.maker for car in responded_API_object.get_carlist()),
     }
+    print(set(car.maker for car in responded_API_object.get_carlist()))
     return render(request, "pages/index.html", context)
 
 
 def get_car_image(request):
-    files = os.listdir('base/static/img')
+    name_to_img_dict = {}
+    file_list = [filename for filename in os.listdir(CarMap.CAR_IMG_DIR) if not filename.startswith('.')]
+    for file in file_list:
+        # getting rid of ".jpg"
+        name_to_img_dict[file[:-4]] = file
     context = {
-        'files': files,
+        'files': name_to_img_dict,
     }
     return render(request, "pages/carList.html", context)
 
@@ -31,6 +37,7 @@ def get_model_list(request, car_model):
 
     context = {
         'cars': car_model_list,
+        'car_name': car_model_list[0].maker,
         'car_img_jpg': CarMap.CAR_TO_IMG[car_model_list[0].maker]
     }
     return render(request, "pages/modelList.html", context)
