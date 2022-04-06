46f8f098e0ceb5976b3642b9ed64b5031f2f32e2
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-04-07 00:19:59 +0900

    added STATICFILES_DIRS

--- carInformation/settings.py
+++ carInformation/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # my app
     'base',
 ]
 
@@ -118,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
