e75f6e44d2c28f9f8f48facdcdfdb26d08a08f22
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-04-06 19:52:49 +0900

    added app_name

--- base/urls.py
+++ base/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = 'base'
+
+
 urlpatterns = [
     path('index', views.index, name='index'),
-    path('', views.get_car_image, name='carImage'),
-    path('modelList/<str:car_model>', views.get_model_list, name='carModel'),
+    path('', views.get_car_image, name='car_image'),
+    path('modelList/<str:car_model>/', views.get_model_list, name='modelList'),
 ]
