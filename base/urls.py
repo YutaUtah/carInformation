630ae4178412d7b18cdc9ff17d9762f7faabcfe0
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-04-06 14:48:05 +0900

    added modelList route

--- base/urls.py
+++ base/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('index', views.index, name='index'),
-    path('', views.get_car_image, name='carimage'),
+    path('', views.get_car_image, name='carImage'),
+    path('modelList/<str:car_model>', views.get_model_list, name='carModel'),
 ]
